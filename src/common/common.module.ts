184e42819a7feafcd457908afc5117bb503b4cea
dinhhaidoan <2200000477@nttu.edu.vn>
2026-01-28 22:45:11 +0700

    base 3

--- src/common/common.module.ts
+++
@@ -1,8 +0,0 @@
-// src/common/common.module.ts
-import { Module } from '@nestjs/common';
-
-@Module({
-  providers: [],
-  exports: [],
-})
-export class CommonModule {}
--- src/config/database.config.ts
+++
@@ -1,6 +0,0 @@
-// src/config/database.config.ts
-export default () => ({
-  database: {
-    url: process.env.DATABASE_URL,
-  },
-});
